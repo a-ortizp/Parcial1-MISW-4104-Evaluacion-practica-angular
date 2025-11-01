a0cbbcfdc1508d7382239ff699a34c0c1941d04a
alejo <alejortizp@gmail.com>
2025-11-01 18:16:09 -0500

    creacion del servicio de vehiculo

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,8 +1,10 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { VehiculoModule } from './vehiculo/vehiculo.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    VehiculoModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
--- src/app/vehiculo/vehiculo.module.ts
+++ src/app/vehiculo/vehiculo.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VehiculoComponent } from './vehiculo.component';
+import { VehiculoService } from './vehiculo.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [VehiculoComponent]
+  declarations: [VehiculoComponent],
+  providers: [VehiculoService],
+  exports: [VehiculoComponent]
 })
 export class VehiculoModule { }
