05427143ca30e8eab787466a21da69eff25023f9
alejo <alejortizp@gmail.com>
2025-11-01 18:07:05 -0500

    creacion de modulo vehiculo y clase vehiculo

---
+++ src/app/vehiculo/vehiculo.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Vehiculo } from './vehiculo';
+import { VehiculoService } from './vehiculo.service';
+
+@Component({
+  selector: 'app-vehiculo',
+  templateUrl: './vehiculo.component.html',
+  styleUrls: ['./vehiculo.component.css']
+})
+export class VehiculoComponent implements OnInit {
+  vehiculos: Array<Vehiculo> = [];
+
+  constructor(private vehiculoService: VehiculoService) { }
+
+  ngOnInit() {
+    this.getVehiculos();
+  }
+
+  getVehiculos(): void {
+    this.vehiculoService.getVehiculos().subscribe(vehiculos => {
+      this.vehiculos = vehiculos;
+    });
+  }
+}
