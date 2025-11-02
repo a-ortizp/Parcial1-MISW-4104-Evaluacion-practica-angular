b4838fb804faa24ad281b5fd5cea3c94f1db549f
alejo <alejortizp@gmail.com>
2025-11-01 19:19:10 -0500

    creacion funcion calculadore de carros por marca

--- src/app/vehiculo/vehiculo.component.ts
+++ src/app/vehiculo/vehiculo.component.ts
@@ -10,6 +10,7 @@ import { VehiculoService } from './vehiculo.service';
 })
 export class VehiculoComponent implements OnInit {
   vehiculos: Array<Vehiculo> = [];
+  conteoMarcas: { [marca: string]: number } = {};
 
   constructor(private vehiculoService: VehiculoService) { }
 
@@ -20,6 +21,23 @@ export class VehiculoComponent implements OnInit {
   getVehiculos(): void {
     this.vehiculoService.getVehiculos().subscribe(vehiculos => {
       this.vehiculos = vehiculos;
+      this.calcularConteoMarcas();
     });
+  }
+
+  calcularConteoMarcas(): void {
+    this.conteoMarcas = {};
+    
+    this.vehiculos.forEach(vehiculo => {
+      if (this.conteoMarcas[vehiculo.marca]) {
+        this.conteoMarcas[vehiculo.marca]++;
+      } else {
+        this.conteoMarcas[vehiculo.marca] = 1;
+      }
+    });
+  }
+
+  getMarcas(): string[] {
+    return Object.keys(this.conteoMarcas);
   }
 }
